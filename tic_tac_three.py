cebae6a46c08e3ae786474ee3d3476903822b538
Tucker <73323523+tucker-rg@users.noreply.github.com>
2022-05-16 14:01:54 -0400

    Update tic_tac_three.py

--- tic_tac_three.py
+++ tic_tac_three.py
@@ -1,5 +1,6 @@
 #practice round 3
 #structure still seems sloppy. turn count seems sloppy. still boring. dumb computer.
+#string input can be improved. error handling.
 
 import random
 
@@ -12,21 +13,9 @@ def print_board(board_name):
     for i in range(3):
         print(' '.join(board_name[i]))
 
-def rotate_board(board_name):
-    '''with some help from stackoverflow'''
-    new_board = list(zip(*reversed(board)))
-    return new_board
-
-def check_diagonal(board_name):
-    '''I'm sure there is a neater way to do this by shuffling the board and reusing the check_win function'''
-    if board_name[0][0] == board_name[1][1] and board_name[0][0] == board_name[2][2]:
-        check_winner(board_name[0][0])
-    else:
-        pass
-
 def player_turn(turn_num):
     '''prompts basic string input from user'''
-    row, column = input("choose a row (top, middle, or bottom)"), input("choose a column (left, middle, or right")
+    row, column = input("choose a row (top, middle, or bottom): ").lower(), input("choose a column (left, middle, or right: ").lower()
     if row not in rows or column not in columns:
         print("I didn't understand. Choose again")
         player_turn(turn_num)
@@ -52,6 +41,8 @@ def check_move(x_axis, y_axis):
     '''see if move is already taken'''
     if board[rows[x_axis]][columns[y_axis]] != "_":
         return True
+    else:
+        pass
 
 def check_winner(item):
     '''see if winner is X or O'''
@@ -72,6 +63,18 @@ def check_rows(board_name):
         else:
             pass
 
+def rotate_board(board_name):
+    '''fixed with some help from stackoverflow'''
+    new_board = list(zip(*reversed(board)))
+    return new_board
+
+def check_diagonal(board_name):
+    '''I'm sure there is a neater way to do this by shuffling the board and reusing the check_win function'''
+    if board_name[0][0] == board_name[1][1] and board_name[0][0] == board_name[2][2]:
+        check_winner(board_name[0][0])
+    else:
+        pass
+
 def check_win(board_names, turn_num):
     '''so many functions for checking if someone has won! this one combines the subfunctions'''
     for i in board_names:
@@ -80,6 +83,8 @@ def check_win(board_names, turn_num):
     if turn_num >= 9:
             print("stalemate")
             quit()
+    else:
+        pass
 
 def main():
     turn = 0
